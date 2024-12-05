5b5e56f9c525a7d36060e5acd4fc06b51e47d3b9
marcinstep-p <marcinstep@gmail.com>
2024-12-05 15:26:53 +0100

    Fix

---
+++ modules/firewall/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  firewall = [
+    {
+      protocol         = "tcp"
+      port_range       = "22"
+      source_addresses = ["0.0.0.0/0"]
+    },
+    {
+      protocol         = "tcp"
+      port_range       = "80"
+      source_addresses = ["0.0.0.0/0"]
+    },
+    {
+      protocol         = "tcp"
+      port_range       = "443"
+      source_addresses = ["0.0.0.0/0"]
+    }
+  ]
+}
