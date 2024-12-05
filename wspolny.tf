7b918f3f3b9042760414a58cd9e3e99320bc9a18
tomasz <t.szymanski94@gmail.com>
2024-12-05 15:29:38 +0100

    Modules usage in wspolny.tf

--- wspolny.tf
+++ wspolny.tf
@@ -5,3 +5,11 @@ module "project" {
 module "ssh_tls_module" {
   source = "./modules/tls-module"
 }
+
+module "vm" {
+  source     = "./modules/vm"
+
+  ssh_key_id = module.ssh_tls_module.open_ssh_key_id
+  #TODO after adding VPC module
+  vpc_uuid   = ""
+}
