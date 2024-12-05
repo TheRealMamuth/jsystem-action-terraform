516ce517129874178eff5f6b911a144469187ab0
tomasz <t.szymanski94@gmail.com>
2024-12-05 14:50:46 +0100

    Add inputs

--- modules/tls-module/main.tf
+++ modules/tls-module/main.tf
@@ -1,9 +1,9 @@
 resource "tls_private_key" "tls-rsa-4096-private-key" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  rsa_bits  = var.tls_rsa_key_length
 }
 
 resource "digitalocean_ssh_key" "open-ssh-key" {
-  name       = var.ssh-key-name
+  name       = var.ssh_key_name
   public_key = tls_private_key.tls-rsa-4096-private-key.public_key_openssh
 }
--- modules/tls-module/variables.tf
+++ modules/tls-module/variables.tf
@@ -1,5 +1,11 @@
-variable "ssh-key-name" {
+variable "ssh_key_name" {
   description = "DigitalOcean SSH key name"
   type        = string
   default     = concat("digitalocean-ssh-key-", uuid())
+}
+
+variable "tls_rsa_key_length" {
+  description = "TLS key length"
+  type        = number
+  default     = 4096
 }
