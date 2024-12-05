945fdeb62250024b4f4d80871aee1600ce7ec4d5
Zdrojek <dr.szuflada@gmail.com>
2024-12-05 15:36:08 +0100

    korekta zmiennych

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,10 +1,10 @@
 resource "digitalocean_droplet" "vm" {
   for_each = var.vms
 
-  name     = "${var.prefix}-vm-${each.value.name}"
+  name     = "${each.key}-vm-${each.value.name}"
   image    = each.value.image
-  region   = var.region
-  size     = var.vm_size
+  region   = each.value.region
+  size     = each.value.vm_size
   vpc_uuid = var.vpc_uuid
   ssh_keys = [var.ssh_key_id]
 }
